cba5e65d57a3a34abd4180126c9b707825c7e3bb
Kelsey Jordahl <kjordahl@alum.mit.edu>
2011-09-21 15:55:32 -0400

    Added plotting to GUI

--- wattui.py
+++ wattui.py
@@ -10,7 +10,7 @@ http://github.enthought.com/traitsui/tutorials/traits_ui_scientific_app.html
 Author: Kelsey Jordahl
 Copyright: Kelsey Jordahl 2011
 License: GPLv3
-Time-stamp: <Tue Sep 20 21:54:15 EDT 2011>
+Time-stamp: <Wed Sep 21 14:18:26 EDT 2011>
 
     This program is free software: you can redistribute it and/or
     modify it under the terms of the GNU General Public License as
@@ -32,11 +32,9 @@ import argparse
 import curses
 from platform import uname
 import numpy as np
-#import matplotlib.pyplot as plt
 
 import wx
 import matplotlib
-# We want matplotlib to use a wxPython backend
 matplotlib.use('WXAgg')
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -45,7 +43,9 @@ from matplotlib.backends.backend_wx import NavigationToolbar2Wx
 from threading import Thread
 from time import sleep
 from traits.api import HasTraits, Instance, Int, Bool, Float, Enum, String, Button, Event
-from traitsui.api import View, Item, ButtonEditor, HGroup, Handler, UIInfo, spring
+from traitsui.api import View, Item, ButtonEditor, Group, Handler, UIInfo, HSplit, spring
+from traitsui.wx.editor import Editor
+from traitsui.wx.basic_editor_factory import BasicEditorFactory
 from traitsui.wx.extra.led_editor import LEDEditor
 
 EXTERNAL_MODE = 'E'
@@ -55,6 +55,35 @@ FULLHANDLING = 2
 
 SIM = True                              # TODO: set this in UI (or guess it)
 
+class _MPLFigureEditor(Editor):
+
+    scrollable  = True
+
+    def init(self, parent):
+        self.control = self._create_canvas(parent)
+        self.set_tooltip()
+
+    def update_editor(self):
+        pass
+
+    def _create_canvas(self, parent):
+        """ Create the MPL canvas. """
+        # The panel lets us add additional controls.
+        panel = wx.Panel(parent, -1, style=wx.CLIP_CHILDREN)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        panel.SetSizer(sizer)
+        # matplotlib commands to create a canvas
+        mpl_control = FigureCanvas(panel, -1, self.value)
+        sizer.Add(mpl_control, 1, wx.LEFT | wx.TOP | wx.GROW)
+        toolbar = NavigationToolbar2Wx(mpl_control)
+        sizer.Add(toolbar, 0, wx.EXPAND)
+        self.value.canvas.SetMinSize((400,300))
+        return panel
+
+class MPLFigureEditor(BasicEditorFactory):
+
+    klass = _MPLFigureEditor
+
 class LoggingThread(Thread):
     def run(self):
         if SIM:
@@ -63,19 +92,22 @@ class LoggingThread(Thread):
             self.s = serial.Serial(port, 115200 )
         print 'Logging started\n'
         n = 0
+        p = []
         while not self.wants_abort:
             line = self.s.readline()
             if line.startswith( '#d' ):
                 fields = line.split(',')
                 if len(fields)>5:
                     W = float(fields[3]) / 10;
+                    p.append(W)
                     V = float(fields[4]) / 10;
                     A = float(fields[5]) / 1000;
                     self.update_data(W, V, A)
+                    self.plot_power(p)
                     #print n, W, V, A
                     n += 1
             if SIM:
-                time.sleep(1)
+                time.sleep(1 / args.speedup)
             #self.display.string = '%d \n' % n
         self.s.close()
         print 'Logging stopped\n'
@@ -105,14 +137,19 @@ class WattsUp( HasTraits ):
     button_label = String('Start')
     start = Event
     logging_thread = Instance(LoggingThread)
+    figure = Instance(Figure, ())
 
     def update_data(self, W, V, A):
         self.power = W
         self.voltage = V
         self.current = A
-        
+
+    def _figure_default(self):
+        figure = Figure()
+        figure.add_axes([0.1, 0.15, 0.5, 0.75])
+        return figure
+    
     def _start_fired(self):
-        #self.interval +=1
         if self.logging_thread and self.logging_thread.isAlive():
             self.logging_thread.wants_abort = True
             self.button_label = 'Start'
@@ -120,32 +157,36 @@ class WattsUp( HasTraits ):
             self.logging_thread = LoggingThread()
             self.logging_thread.wants_abort = False
             self.logging_thread.update_data = self.update_data
+            self.logging_thread.plot_power = self.plot_power
+            self.logging_thread.figure = self.figure
             self.logging_thread.start()
             self.button_label = 'Stop'
 
-    view = View(Item('power', height = -40), Item('voltage', height = -40),
+    def plot_power(self,p):
+        t = np.linspace(0,len(p)/60.0,len(p)) # should multiply by interval
+        # print t.size, len(p)
+        self.figure.axes[0].clear()
+        self.figure.axes[0].plot(t,p,'r')
+        self.figure.axes[0].set_xlabel('Time (minutes)')
+        self.figure.axes[0].set_ylabel('Power (W)')
+        wx.CallAfter(self.figure.canvas.draw)
+
+    view = View(HSplit(Group(Item('power', height = -40), Item('voltage', height = -40),
                 Item('current', height = -40),
                 'interval', 'mode', 'port',
                 Item('start', label = 'Logging',
-                     editor = ButtonEditor(label_value = 'button_label')))
+                     editor = ButtonEditor(label_value = 'button_label'))),
+                Item('figure', editor=MPLFigureEditor(),
+                            dock='vertical', height = -400, width = -800,
+                            resizable=True),
+                            show_labels=False))
 
 def main(args):
     WattsUp().configure_traits()
     wx.PySimpleApp().MainLoop()
 
 if __name__ == '__main__':
-    # args not currently used in GUI program
     parser = argparse.ArgumentParser(description='Get data from Watts Up power meter.')
-    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', help='verbose')
-    parser.add_argument('-d', '--debug', dest='debug', action='store_true', help='debugging output')
-    parser.add_argument('-m', '--simulation-mode', dest='sim', action='store_true', help='simulate logging by reading serial data from disk with delay of sample interval between lines')
-    parser.add_argument('-i', '--internal-mode', dest='internal', action='store_true', help='Set meter to internal logging mode')
-    parser.add_argument('-f', '--fetch', dest='fetch', action='store_true', help='Fetch data stored on the meter (NOT YET WORKING!)')
-    parser.add_argument('-g', '--graphics-mode', dest='plot', action='store_true', help='Graphical output: plot the data (NOT YET WORKING!)')
-    parser.add_argument('-l', '--log', dest='log', action='store_true', help='log data in real time')
-    parser.add_argument('-r', '--raw', dest='raw', action='store_true', help='output raw file')
-    parser.add_argument('-o', '--outfile', dest='outfile', default='log.out', help='Output file')
-    parser.add_argument('-s', '--sample-interval', dest='interval', default=1, help='Sample interval (default 1 s)')
-    parser.add_argument('-p', '--port', dest='port', default=None, help='USB serial port')
+    parser.add_argument('-s', '--speedup', dest='speedup', default=1, type=float, help='Speed up simulation by factor (default 1)')
     args = parser.parse_args()
     main(args)
